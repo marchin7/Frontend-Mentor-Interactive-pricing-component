528c70f06df54534210d65b58600d7f8ca846738
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-03-15 23:17:40 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const slider = document.querySelector(".slider");
+const vars = document.querySelector(":root");
+const pageViews = document.querySelector("#pageviews");
+const form = document.querySelector("form");
+const price = document.querySelector("#price");
+const toggleSwitch = document.querySelector("#toggle-switch");
+
+calculate();
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+toggleSwitch.addEventListener("input", () => {
+    calculate();
+});
+
+slider.addEventListener("input", calculate);
+
+function calculate() {
+    let sliderPercentage = `${slider.value * 25 - 25}%`;
+    vars.style.setProperty("--progres-bar", sliderPercentage);
+
+    if (toggleSwitch.value == 0) {
+        if (slider.value == 1) {
+            getResult("10K", "$8.00");
+        } else if (slider.value == 2) {
+            getResult("50K", "$12.00");
+        } else if (slider.value == 3) {
+            getResult("100K", "$16.00");
+        } else if (slider.value == 4) {
+            getResult("500K", "$24.00");
+        } else if (slider.value == 5) {
+            getResult("1M", "$36.00");
+        }
+    } else {
+        if (slider.value == 1  ) {
+            getResult("10K", "$6.00");
+        } else if (slider.value == 2) {
+            getResult("50K", "$9.00");
+        } else if (slider.value == 3) {
+            getResult("100K", "$12.00");
+        } else if (slider.value == 4) {
+            getResult("500K", "$18.00");
+        } else if (slider.value == 5) {
+            getResult("1M", "$27.00");
+        }
+    }
+
+    function getResult(pages, $price) {
+        pageViews.innerHTML = pages;
+        price.innerHTML = $price;
+    }
+}
+
+
+
+//let myVar = getComputedStyle(vars)

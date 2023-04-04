4860396868d9ce336a8dfbf8f55b986d6bd061ee
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-04-04 09:30:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,34 +8,34 @@ const labelMonthly = document.querySelector(".label-monthly");
 const labelYearly = document.querySelector(".label-yearly");
 
 let pricePlan = [
-    { pages: "10K", price: 8 },
-    { pages: "50K", price: 12 },
-    { pages: "100K", price: 16 },
-    { pages: "500K", price: 24 },
-    { pages: "1M", price: 36 },
+    { pageViews: "10K", price: 8 },
+    { pageViews: "50K", price: 12 },
+    { pageViews: "100K", price: 16 },
+    { pageViews: "500K", price: 24 },
+    { pageViews: "1M", price: 36 },
 ];
 
 let discountPercent = 25;
 
-//calculate();
+getResult();
 
 form.addEventListener("input", (e) => {
     e.preventDefault();
-    calculate();
+    getResult();
 });
 
 toggleSwitch.addEventListener("input", () => {
-    calculate();
+    getResult();
 });
 
 labelMonthly.addEventListener("click", () => {
     toggleSwitch.value = "0";
-    calculate();
+    getResult();
 });
 
 labelYearly.addEventListener("click", () => {
     toggleSwitch.value = "1";
-    calculate();
+    getResult();
 });
 
 function updateProgresBarColor() {
@@ -43,27 +43,26 @@ function updateProgresBarColor() {
     vars.style.setProperty("--progres-bar", sliderProgres);
 }
 
-//slider.addEventListener("input", calculate);
-
-function calculate() {
+function getResult() {
     let index = slider.value - 1;
     if (toggleSwitch.value == 0) {
-        getResult(
-            pricePlan[index].pages,
+        render(
+            pricePlan[index].pageViews,
             `${pricePlan[index].price.toLocaleString("en-US", { style: "currency", currency: "USD" })}`
         );
     } else {
-        getResult(
-            pricePlan[index].pages,
+        render(
+            pricePlan[index].pageViews,
             `${(pricePlan[index].price - (pricePlan[index].price / 100) * discountPercent).toLocaleString("en-US", {
-                style: "currency", currency: "USD",
+                style: "currency",
+                currency: "USD",
             })}`
         );
     }
     updateProgresBarColor();
 }
 
-function getResult($pages, $price) {
+function render($pages, $price) {
     pageViews.innerText = $pages;
     price.innerText = $price;
 }
